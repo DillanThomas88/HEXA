22af6226dd230884e32ae9504d323e908a701cfc
DillanThomas <mansor.dillan@gmail.com>
2022-01-17 04:56:50 -0800

    fixed js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./public/index.html","./public/js/diceroller.js"],
+  content: ["./index.html","./public/js/diceroller.js"],
   theme: {
     extend: {
       fontFamily: {
