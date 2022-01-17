cd9caec3a7139b22a2c112f23999a90d7519f038
DillanThomas <mansor.dillan@gmail.com>
2022-01-17 04:36:35 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/index.html","./public/js/diceroller.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+    },
+  },
+},
+  plugins: [],
+}
