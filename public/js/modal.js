9e7bcca59336fb2d67d5c14b94a1209eef9048d4
DillanThomas <mansor.dillan@gmail.com>
2022-01-18 22:31:37 -0800

    added modal move animation

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,10 @@
 var openmodal = document.querySelectorAll('.modal-open')
 // --------------
 let tem = document.querySelector('.modal')
+let distance = 20
 tem.style.opacity = 0
+tem.style.top = `${distance}px`
+
 // --------------
 for (var i = 0; i < openmodal.length; i++) {
   openmodal[i].addEventListener('click', function(event){
@@ -38,34 +41,61 @@ function toggleModal () {
   modal.classList.toggle('pointer-events-none')
   body.classList.toggle('modal-active')
   modal.classList.toggle('active')
-  modalAnimate()
+  modalFade()
+  modalMove()
 // modal.classList.toggle('opacity-0')
 }
 
 
-function modalAnimate () {
+function modalFade () {
   const modal = document.querySelector('.modal')
-  let duration = 15
+  let duration = 40
   let incriment = 0
+
   if(modal.classList.contains('active')){
+
     incriment = duration
     let timer = setInterval(() => {  
       incriment-- 
-      if(incriment < 0){clearInterval(timer); return}
+      if(incriment <= 0){clearInterval(timer); modal.style.opacity = 0; return}
       modal.style.opacity = `${incriment/duration}`
-      console.log(incriment/duration)
+      // console.log(incriment/duration)
     }, 1);
-
 
   } else {
 
     let timer = setInterval(() => {
       incriment++
-      if(incriment > duration){clearInterval(timer); return}
+      if(incriment > duration){clearInterval(timer);modal.style.opacity = duration; return}
       modal.style.opacity = `${incriment/duration}`
-      console.log(incriment/duration)
+      // console.log(incriment/duration)
     }, 1);
   }
 }
 
+function modalMove () {
+  const modal = document.querySelector('.modal')
+  if(modal.classList.contains('active')){
+    let incriment = 0
+    let timer = setInterval(() => {
+      console.log('hey')
+      incriment++
+      if(incriment >= distance){clearInterval(timer); modal.style.top = `${distance}px`; return}
+      modal.style.top = `${incriment}px`
+    }, 1);
+    
+    
+    
+  } else {
+    
+    incriment = distance
+    
+    let timer = setInterval(() => {  
+      incriment-- 
+      if(incriment < 0){clearInterval(timer); modal.style.top = 0; return}
+      modal.style.top = `${incriment}px`
+      // console.log(incriment)
+    }, 1);
+  }
+}
  
