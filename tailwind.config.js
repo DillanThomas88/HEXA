b8faea4bfd91aaf60bc5ac28cdc88cbc32a37d4a
DillanThomas <mansor.dillan@gmail.com>
2022-01-20 12:49:43 -0800

    added 12 Pro support

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
       fontFamily: {
         'montserrat': ['Montserrat'],
     },
+    screens: {
+      '13mini': '375px',
+
+      '12pro': '390px',
+
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    }
   },
 },
   plugins: [],
